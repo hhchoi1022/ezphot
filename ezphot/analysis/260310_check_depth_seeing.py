4cd32004ce51c12ab80c2031d9ab56c102cb7926
hhchoi1022 <hhchoi1022@gmail.com>
2026-06-23 23:34:28 +0900

    260623_WFM

--- ezphot/analysis/260310_check_depth_seeing.py
+++ ezphot/analysis/260310_check_depth_seeing.py
@@ -1,10 +1,16 @@
 #%%
 from bridge.connector import GWPortalConnector
 #%%
-gwconnector = GWPortalConnector('combined')
+gwconnector = GWPortalConnector('processed')
 # %%
-tbl_recent = gwconnector.query(obs_start_date = '2025-01-01', obs_end_date = '2026-01-01')
 
+# tbl_recent = gwconnector.query(since_days = 550)
+# tbl_recent = gwconnector.query(obs_start_date = '2025-01-01', obs_end_date = '2026-06-20')
+#%%
+tbl_recent = tbl_recent[tbl_recent['exptime'] == 100]
+#%%
+tbl_2026 = tbl_recent[[obstime.startswith('2026') for obstime in tbl_recent['obstime']]]
+tbl_2025 = tbl_recent[[obstime.startswith('2025') for obstime in tbl_recent['obstime']]]
 #%%
 
 from astropy.stats import sigma_clip
@@ -46,7 +52,16 @@ sns.set_style("whitegrid")
 
 plt.figure(figsize=(10,5))
 
-palette = sns.color_palette("Spectral", len(filter_order))
+base_colors = sns.color_palette("Spectral", len(filter_order))
+
+palette = dict(zip(filter_order, base_colors))
+
+palette.update({
+    'g': 'green',
+    'r': 'red',
+    'i': 'yellow',
+    'z': 'black'
+})
 
 ax = sns.violinplot(
     data=df_ul5,
@@ -55,8 +70,8 @@ ax = sns.violinplot(
     order=filter_order,
     palette=palette,
     inner=None,
-    cut=0,
-    linewidth=1
+    cut= 0,
+    linewidth=1,
 )
 
 medians = df.groupby('filter')['ul5'].median()
@@ -68,7 +83,8 @@ for i, f in enumerate(filter_order):
         continue
 
     median = medians[f]
-    ymax = vals.max()
+    # ymax is 99 percentile of the values
+    ymax = vals.quantile(0.999)
 
     ax.scatter(
         i, median,
@@ -80,19 +96,101 @@ for i, f in enumerate(filter_order):
 
     ax.text(
         i,
-        ymax + 0.05,
+        median + 1.7,
         f"{median:.2f}",
         ha='center',
-        fontsize=10,
+        fontsize=12,
         rotation=90,
-        color=palette[i]
+        color='k'
     )
 
-ax.set_ylabel("Limiting magnitude (5σ)")
-ax.set_xlabel("")
-ax.set_title("Limiting magnitude distribution (exptime=100s)")
+ax.set_ylim(15, 24)
 
-plt.xticks(rotation=45)
+ax.set_ylabel("Limiting magnitude (5σ)", fontsize = 14)
+ax.set_xlabel("")
+ax.set_title("Limiting magnitude distribution (exptime=100s)", fontsize = 16)
+
+plt.xticks(rotation=45, fontsize = 12)
+plt.yticks(fontsize = 12)
+
+plt.tight_layout()
+plt.show()
+# %%
+
+
+df = tbl_2026.to_pandas()
+df_ul5 = sigma_clip_by_filter(df, 'seeing', sigma=3)
+
+filter_order = [
+'g','r','i','z',
+'m400','m425','m450','m475','m500','m525','m550','m575',
+'m600','m625','m650','m675','m700','m725','m750','m775',
+'m800','m825','m850','m875'
+]
+
+sns.set_style("whitegrid")
+
+plt.figure(figsize=(10,5))
+
+base_colors = sns.color_palette("Spectral", len(filter_order))
+
+palette = dict(zip(filter_order, base_colors))
+
+palette.update({
+    'g': 'green',
+    'r': 'red',
+    'i': 'yellow',
+    'z': 'black'
+})
+
+ax = sns.violinplot(
+    data=df_ul5,
+    x='filter',
+    y='seeing',
+    order=filter_order,
+    palette=palette,
+    inner=None,
+    cut=0,
+    linewidth=1
+)
+
+medians = df_ul5.groupby('filter')['seeing'].median()
+
+for i, f in enumerate(filter_order):
+
+    vals = df_ul5[df_ul5['filter']==f]['seeing']
+    if len(vals)==0:
+        continue
+
+    median = medians[f]
+    # ymax is 99 percentile of the values
+    ymax = vals.quantile(0.995)
+
+    ax.scatter(
+        i, median,
+        color='white',
+        edgecolor='black',
+        s=80,
+        zorder=3
+    )
+
+    ax.text(
+        i,
+        ymax + 0.6,
+        f"{median:.2f}",
+        ha='center',
+        fontsize=12,
+        rotation=90,
+        color='k'
+    )
+
+ax.set_ylim(1.0, 7)
+ax.set_ylabel("Seeing [arcsec]", fontsize = 14)
+ax.set_xlabel("")
+ax.set_title("Seeing distribution", fontsize = 16)
+
+plt.xticks(rotation=45, fontsize = 12)
+plt.yticks(fontsize = 12)
 
 plt.tight_layout()
 plt.show()
--- ezphot/analysis/260618_calspec_process.py
+++ ezphot/analysis/260618_calspec_process.py
@@ -29,61 +29,61 @@ calspec_info_observed = calspec_info[calspec_info['is_observed'] == 'True']
 #%%
 check_later_objects = []
 #%%
-all_num_images = dict()
-all_num_paths = dict()
-for target_row in calspec_info_observed:
-    tile_id = target_row['tileid']
-    ra_str = target_row['ra']
-    dec_str = target_row['dec']
-    coord = SkyCoord(ra = ra_str, dec = dec_str, unit = ('hourangle', 'deg'))
-    ra = coord.ra.deg
-    dec = coord.dec.deg
-    objname = target_row['star_names']
+# all_num_images = dict()
+# all_num_paths = dict()
+# for target_row in calspec_info_observed:
+#     tile_id = target_row['tileid']
+#     ra_str = target_row['ra']
+#     dec_str = target_row['dec']
+#     coord = SkyCoord(ra = ra_str, dec = dec_str, unit = ('hourangle', 'deg'))
+#     ra = coord.ra.deg
+#     dec = coord.dec.deg
+#     objname = target_row['star_names']
 
-    from bridge.alertmonitor import AlertProcessor
-    from bridge.objects import Alert
-    from astropy.time import Time
-    alert_instance = Alert(objname = objname, ra = ra, dec = dec, trigger_time = Time('2001-01-01'), alert_time = Time('2001-01-01'))
-    alertprocessor = AlertProcessor()
-    alertprocessor.load_images_db(alert_instance = alert_instance)
-    from ezphot.utils import DataBrowser
-    dbrowser = DataBrowser('scidata')
-    dbrowser.objname = tile_id
-    target_pathlist = dbrowser.search(pattern = '*.fits', return_type = 'path')
-    dbrowser.objname = objname
-    target_pathlist.extend(dbrowser.search(pattern = '*.fits', return_type = 'path'))
+#     from bridge.alertmonitor import AlertProcessor
+#     from bridge.objects import Alert
+#     from astropy.time import Time
+#     alert_instance = Alert(objname = objname, ra = ra, dec = dec, trigger_time = Time('2001-01-01'), alert_time = Time('2001-01-01'))
+#     alertprocessor = AlertProcessor()
+#     alertprocessor.load_images_db(alert_instance = alert_instance)
+#     from ezphot.utils import DataBrowser
+#     dbrowser = DataBrowser('scidata')
+#     dbrowser.objname = tile_id
+#     target_pathlist = dbrowser.search(pattern = '*.fits', return_type = 'path')
+#     dbrowser.objname = objname
+#     target_pathlist.extend(dbrowser.search(pattern = '*.fits', return_type = 'path'))
 
-    all_num_images[objname] = len(alertprocessor.target_images)
-    all_num_paths[objname] = len(target_pathlist)
-    try:
-        import numpy as np
-        from ezphot.imageobjects import ImageSet
-        all_exposure_times = np.array([target_img.exptime for target_img in alertprocessor.target_images])
-        exposure_set = set(all_exposure_times)
-        target_imgset = ImageSet(alertprocessor.target_images)
-        for exposure_time in exposure_set:
-            target_imgset.select_images(exptime = exposure_time)
-            alertprocessor_to_process = AlertProcessor()
-            alertprocessor_to_process.target_images = target_imgset.target_images
-            alertprocessor_to_process.config.max_workers = 48
-            alertprocessor_to_process.config.single_process['do_calculate_bkgrms_from_propagation'] = False
-            alertprocessor_to_process.config.single_process['do_forced_photometry'] = True
-            exptime_ratio = exposure_time / 100
-            magnitude_offset = 2.5*np.log10(np.sqrt(exptime_ratio))
-            alertprocessor_to_process.config.photcal['mag_range_default'] = list(np.array(alertprocessor_to_process.config.photcal['mag_range_default']) + magnitude_offset)
-            for filter_, val in alertprocessor_to_process.config.photcal['mag_range_dict'].items():
-                alertprocessor_to_process.config.photcal['mag_range_dict'][filter_] = list(np.array(val) + magnitude_offset)
-            alertprocessor_to_process.pipeline_before_stacking(alert_instance = alert_instance)
-    except Exception as e:
-        check_later_objects.append(objname)
-        print(f"Error processing {objname}: {e}")
+#     all_num_images[objname] = len(alertprocessor.target_images)
+#     all_num_paths[objname] = len(target_pathlist)
+#     try:
+#         import numpy as np
+#         from ezphot.imageobjects import ImageSet
+#         all_exposure_times = np.array([target_img.exptime for target_img in alertprocessor.target_images])
+#         exposure_set = set(all_exposure_times)
+#         target_imgset = ImageSet(alertprocessor.target_images)
+#         for exposure_time in exposure_set:
+#             target_imgset.select_images(exptime = exposure_time)
+#             alertprocessor_to_process = AlertProcessor()
+#             alertprocessor_to_process.target_images = target_imgset.target_images
+#             alertprocessor_to_process.config.max_workers = 48
+#             alertprocessor_to_process.config.single_process['do_calculate_bkgrms_from_propagation'] = False
+#             alertprocessor_to_process.config.single_process['do_forced_photometry'] = True
+#             exptime_ratio = exposure_time / 100
+#             magnitude_offset = 2.5*np.log10(np.sqrt(exptime_ratio))
+#             alertprocessor_to_process.config.photcal['mag_range_default'] = list(np.array(alertprocessor_to_process.config.photcal['mag_range_default']) + magnitude_offset)
+#             for filter_, val in alertprocessor_to_process.config.photcal['mag_range_dict'].items():
+#                 alertprocessor_to_process.config.photcal['mag_range_dict'][filter_] = list(np.array(val) + magnitude_offset)
+#             alertprocessor_to_process.pipeline_before_stacking(alert_instance = alert_instance)
+#     except Exception as e:
+#         check_later_objects.append(objname)
+#         print(f"Error processing {objname}: {e}")
 # %%
 import numpy as np
 from bridge.alertmonitor import AlertProcessor
 from bridge.objects import Alert
 from astropy.time import Time
 from ezphot.imageobjects import ImageSet
-target_row = calspec_info_observed[1]
+target_row = calspec_info_observed[2]
 tile_id = target_row['tileid']
 ra_str = target_row['ra']
 dec_str = target_row['dec']
@@ -97,21 +97,14 @@ alertprocessor.load_images_ezphot(alert_instance = alert_instance, file_pattern 
 target_imgset = ImageSet(alertprocessor.target_images)
 target_imgsetlist = target_imgset.divide_images(by_exptime = True)
 #%%
-%matplotlib inline
-for target_imgset in target_imgsetlist:
-    if len(target_imgset.target_images) > 5:
-        target_imgset.select_quality_images(
-            visualize = True,
-        )
-    target_imgset.prepare_stack(n_proc = 32, zp_key = 'ZP_APER_3', verbose = False)
-    result = target_imgset.stack(n_proc = 8, remove_intermediate = True, verbose = False)
-#%%
-alertprocessor.stacking()
-
+alertprocessor.stacking(by_filter = True, by_exptime = True)
 # %%
-alertprocessor.target_images = [result[0]]
 alertprocessor.pipeline_after_stacking(alert_instance = alert_instance)
-# %%
+#%%
+alertprocessor.config
+
+
+#%%
 from ezphot.utils import DataBrowser
 dbrowser = DataBrowser('scidata')
 dbrowser.objname = tile_id
